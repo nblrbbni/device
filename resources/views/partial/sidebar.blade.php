19df4078bf1f188d844688bd9aff512f1f652e2e
nblrbbni <muhammadrabbani463@smk.belajar.id>
2023-10-24 12:49:39 +0700

    backend-layouting

---
+++ resources/views/partial/sidebar.blade.php
@@ -0,0 +1,59 @@
+<div class="collapse navbar-collapse" id="sidebar-menu">
+    <ul class="navbar-nav pt-lg-3">
+      <hr>
+      <li class="nav-item-active">
+        <a class="nav-link" href="/">
+          <span
+            class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+              stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path>
+              <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+              <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+            </svg>
+          </span>
+          <span class="nav-link-title">
+            Dashboard
+          </span>
+        </a>
+      </li>
+      <hr>
+      <li class="nav-item">
+        <a class="nav-link" href="/electronic">
+          <span
+            class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+              stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M14.5 16h-10.5a1 1 0 0 1 -1 -1v-10a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v7.5"></path>
+              <path d="M7 20h6"></path>
+              <path d="M9 16v4"></path>
+              <path d="M19 16l-2 3h4l-2 3"></path>
+            </svg>
+          </span>
+          <span class="nav-link-title">
+            Elektronik
+          </span>
+        </a>
+      </li>
+      <hr>
+      <li class="nav-item">
+        <a class="nav-link" href="/notification">
+          <span
+            class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+              stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6">
+              </path>
+              <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
+            </svg>
+          </span>
+          <span class="nav-link-title">
+            Information
+          </span>
+        </a>
+      </li>
+    </ul>
+  </div>
